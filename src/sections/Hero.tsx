ddf325c34cd25dcf3f347f2903e5db83d17ab947
Karan <kc2532002@gmail.com>
2025-02-23 14:57:54 +0530

    hero-animation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,11 +1,26 @@
+"use client";
 import ArrowIcon from "@/assets/arrow-right.svg";
 import cogImage from "@/assets/cog.png";
-import Image from "next/image";
 import cylinderImage from "@/assets/cylinder.png";
-import noodle from "@/assets/noodle.png"
+import noodle from "@/assets/noodle.png";
+import { motion, useScroll, useTransform, useMotionValueEvent } from "framer-motion";
+import { useRef } from "react";
+
+
 const Hero = () => {
+  const heroRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: heroRef,
+    offset: ["start end", "end start"]
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
+
+  // To check the values for the  animations
+  // useMotionValueEvent(translateY, 'change',(latestValue) => console.log(latestValue));
+
   return (
-    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left_,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+    <section ref={heroRef} className="pt-8 pb-20 md:pt-8 md:pb-10 lg:mt-20  bg-[radial-gradient(ellipse_200%_100%_at_bottom_left_,#183EC2,#EAEEFE_100%)] overflow-x-clip">
       <div className="container">
         <div className="md:flex items-center">
           <div className="w-[478px]">
@@ -21,9 +36,40 @@ const Hero = () => {
             </div>
           </div>
           <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-            <Image src={cogImage} alt="cog image" className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0" />
-            <Image src={cylinderImage} alt="cylinderimg" height={220} width={220} className="hidden md:block -top-8 -left-32 md:absolute" />
-            <Image src={noodle} alt="noodle" width={220} className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]" />
+            <motion.img
+              src={cogImage.src}
+              alt="cog image"
+              className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+              animate={{
+                translateY: [-30, 30],
+              }}
+              transition={{
+                repeat: Infinity,
+                repeatType: "mirror",
+                duration: 3,
+                ease: "easeInOut"
+              }}
+            />
+            <motion.img
+              src={cylinderImage.src}
+              alt="cylinderimg"
+              height={220}
+              width={220}
+              className="hidden md:block -top-8 -left-32 md:absolute"
+              style={{
+                translateY: translateY,
+              }}
+            />
+            <motion.img
+              src={noodle.src}
+              alt="noodle"
+              width={220}
+              className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
+              style={{
+                rotate: 30,
+                translateY: translateY,
+              }}
+            />
           </div>
         </div>
       </div>
